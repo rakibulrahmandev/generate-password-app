0b81ce24f28a6797d0e1d956543b6579544d224d
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-27 18:15:27 +0600

    javascript necessary variable declare done on 07/27/2025 at 06:15 pm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 //? generate password app ------------------------------------------------------------------->
+
+// let's access html element ------------------------------------->
+const input = document.getElementById('input');
+const copyBtn = document.getElementById('copy-btn');
+const generateBtn = document.getElementById('generate-btn');
+
+// let's declare equipment --------------------------------------->
+let upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+let numbers = '0123456789';
+let symbols = `!@#$%^&*()_+-=[]{}|;:',.<>?/`;
+
