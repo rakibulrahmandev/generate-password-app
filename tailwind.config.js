56cbf3ef671a45a7b63504deb601ffac30481a27
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-27 17:45:47 +0600

    html structure done and add font family in tailwind config file done on 07/27/2025 at 05:45 pm

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
       },
     },
   },
