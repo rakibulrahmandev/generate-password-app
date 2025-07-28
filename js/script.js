58da7a61258a03072ddb7f1025e2bbe6a8f77a4d
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-07-28 10:33:20 +0600

    change some style and favicon and done all functionality with javascript done on 07/28/2025 at 10:33 am

--- js/script.js
+++ js/script.js
@@ -2,12 +2,50 @@
 
 // let's access html element ------------------------------------->
 const input = document.getElementById('input');
-const copyBtn = document.getElementById('copy-btn');
-const generateBtn = document.getElementById('generate-btn');
 
 // let's declare equipment --------------------------------------->
-let upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-let lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
-let numbers = '0123456789';
-let symbols = `!@#$%^&*()_+-=[]{}|;:',.<>?/`;
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const symbols = `!@#$%^&*()_+-=[]{}|;:',.<>?/`;
+const passLength = 12;
+const allCharacter =`${upperCaseLetters}${lowerCaseLetters}${numbers}${symbols}`;
 
+// event listener here ------------------------------------------->
+document.getElementById('generate-btn').addEventListener('click', () => {
+    displayPassword();
+});
+
+document.getElementById('copy-btn').addEventListener('click', () => {
+    copyText();
+});
+
+// function ------------------------------------------------------>
+const generatePassWord = () => {
+    let password = '';
+
+    password += upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    password += lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    for (let i = 0; password.length < passLength; i++) {
+        password += allCharacter[Math.floor(Math.random() * allCharacter.length)];
+    };
+
+    return password;
+};
+
+const displayPassword = () => {
+    input.value = generatePassWord();
+};
+
+const copyText = () => {
+    navigator.clipboard.writeText(input.value)
+    .then(() => {
+        alert('Copied to clipboard!');
+    })
+    .catch((err) => {
+        alert(`Failed to copy:${err}`);
+    });
+};
